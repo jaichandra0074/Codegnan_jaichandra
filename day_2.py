f5be17137e07c2e734c50c0ff13db412ce090d64
jaichandra0074 <chanduneduri7273@gmail.com>
2026-03-19 14:38:02 +0530

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,430 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+<class 'float'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+<class 'float'>
+<class 'int'>
+<class 'complex'>
+(25+3j)
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+Traceback (most recent call last):
+  File "C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py", line 117, in <module>
+    m = float(n)
+TypeError: float() argument must be a string or a real number, not 'complex'
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+True
+<class 'bool'>
+1
+1.0
+0j
+<class 'complex'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+True
+<class 'bool'>
+1
+1.0
+0j
+<class 'complex'>
+29
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+True
+<class 'bool'>
+1
+1.0
+0j
+<class 'complex'>
+29
+5
+enter the value : 25
+25
+<class 'str'>
+enter the value : 25.00
+Traceback (most recent call last):
+  File "C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py", line 151, in <module>
+    a = int(input("enter the value : ")) # only interger input
+ValueError: invalid literal for int() with base 10: '25.00'
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+True
+<class 'bool'>
+1
+1.0
+0j
+<class 'complex'>
+29
+5
+enter the value : 25
+25
+<class 'str'>
+enter the value : 25
+25
+<class 'int'>
+25.00
+25.0
+<class 'float'>
+3+5j
+(3+5j)
+<class 'complex'>
+
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+3
+7
+10
+codegnan visakhapatanam 7 cmo@codenan.com cmo@codenan.com
+21
+20
+22
+21
+21 21 21
+40
+<class 'int'>
+172.25
+<class 'float'>
+(2+3j)
+<class 'complex'>
+27
+<class 'int'>
+25
+<class 'int'>
+25.0
+<class 'float'>
+(25+0j)
+<class 'complex'>
+<class 'float'>
+25
+(25+0j)
+<class 'int'>
+<class 'complex'>
+(25+3j)
+<class 'complex'>
+True
+<class 'bool'>
+1
+1.0
+0j
+<class 'complex'>
+29
+5
+enter the value : 25
+25
+<class 'str'>
+enter the value : 25
+25
+<class 'int'>
+25.00
+25.0
+<class 'float'>
+3+5j
+(3+5j)
+<class 'complex'>
+enter the name of the student : neduri jaichandra dasaradha ramayya
+--------------------
+enter the admission fees : 10000
+enter the tuition fees : 300000
+enter the hostel fees : 20000
+Student Name :  neduri jaichandra dasaradha ramayya
+Admission fees is :  10000
+Tuition fees is :  300000
+Hostel fees is : 20000
+Total fees is : 330000
+-------------------------------
+>>> 
+============================================================== RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_2.py ==============================================================
+enter the name of the student : neduri jaichandra dasaradha ramayya
+--------------------
+enter the admission fees : 10000
+enter the tuition fees pre year : 250000
+enter the hostel fees per year : 50000
+Student Name :  neduri jaichandra dasaradha ramayya
+Admission fees is :  10000
+Tuition fees is :  250000
+Hostel fees is : 50000
+Admission fees for 4 years :  40000
+Tuition fees for 4 years :  1000000
+Hostel fees for 4 years : 200000
+Total fees is : 1210000
+-------------------------------
