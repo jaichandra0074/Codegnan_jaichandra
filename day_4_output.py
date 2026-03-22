fc19f3259d37adf2ae786e35f96cff0790b27b7c
jaichandra0074 <chanduneduri7273@gmail.com>
2026-03-22 18:43:46 +0530

    Add files via upload

---
+++ day_4_output.py
@@ -0,0 +1,50 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_4.py
+8
+2
+15
+1.6666666666666667
+125
+1
+2
+enter the first value  :2
+enter the second value :3
+the result is  20
+the result is 20
+ The result of 2 and 3 is 20
+6
+9
+ the value of 5**3 is 125
+ the value of a after using  **= is 125
+Enter a value:3
+Enter another value:5
+False
+True
+True
+False
+True
+False
+<class 'str'>
+True
+False
+True
+enter a number1
+1
+The number 1 in [12, 6, 3, 4] is False 
+True
+False
+True
+False
+False
+True
+1343551505024
+1343513284864
+False
+True
+1343513284864
+True
+1
+7
+0b101
