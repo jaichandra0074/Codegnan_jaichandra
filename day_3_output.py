1e10c686fb2fb4e1e0b2e42b4635c3f247bfeec2
jaichandra0074 <chanduneduri7273@gmail.com>
2026-03-22 18:30:24 +0530

    Add files via upload

---
+++ day_3_output.py
@@ -0,0 +1,38 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_3.py
+Enter your id 		     : vu21csen0100074
+Enter your name 	     : neduri jaichandra dasaradha ramayya
+Enter your branch 	     : computer science
+Enter your year of studying  : 2021-2025
+
+ Enter each subjects marks 
+
+python marks: 90
+os marks    : 70
+maths marks : 80
+dbms marks  : 70
+
+
+
+___student_details___
+
+Student_Name  :  neduri jaichandra dasaradha ramayya
+Student_Id    :  vu21csen0100074
+Branch	      :  computer science
+year_of_study :  2021-2025
+Exam Results  : 
+        python - Passed
+        os - Passed
+        maths - Passed
+        dbms - Passed
+(3+4j)
+<class 'str'>
+<class 'complex'>
+True
+<class 'bool'>
+3.0
+4.0
+<class 'int'>
+<class 'float'>
