5b77cd69570b90a7b92b5ca982119baebcda55a1
jaichandra0074 <chanduneduri7273@gmail.com>
2026-03-22 18:57:59 +0530

    Add files via upload

---
+++ day_5_output.py
@@ -0,0 +1,15 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/OneDrive/Desktop/codegnan_py/day_5.py
+INSERT YOUR ATM CARD 
+Enter any number between  0 to 99 :25
+Enter your pin :2303
+select any of the below options
+['withdrawl(1)', 'check_balance(2)']
+1
+Enter amount5000
+please collect yoiur cash: 5000
+display balancce (yes(1)/no(0):1
+available balance after withdrawl - 145000
+THANKS FOR USING SBI
