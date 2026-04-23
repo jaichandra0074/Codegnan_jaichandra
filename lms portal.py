a5846f93a67a9dbf393e950fba75b0325e3c4884
jaichandra0074 <chanduneduri7273@gmail.com>
2026-04-23 12:43:35 +0530

    Add files via upload

---
+++ lms portal.py
@@ -0,0 +1,24 @@
+'''
+lms portal of university
+
+
+'''
+
+class User:
+    def __init__(self,name,age,main_depart):
+        self.name = name
+        self.age = age
+        self.main_depart = main_depart
+
+    def display(self):
+        print(f"Name : {self.name}")
+        print(f"Age : {self.age}")
+        print(f"Main_course : {self.main_depart}")
+
+#class students(User):
+  #  def __init__ (self,name):
+        
+    
+        
+        
+    
